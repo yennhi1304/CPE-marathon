d3fe88ca167f87aec6e9171529a6e18277b3eac8
yennhi1304 <yenhi130406@gmail.com>
2025-04-25 16:22:20 +0800

    Update solution.cpp

--- 25-04-2025/Nhi/solution.cpp
+++ 25-04-2025/Nhi/solution.cpp
@@ -16,7 +16,7 @@ int main()
     cin >> N;
     for (int i = 0; i < N; i++)
     {
-        const int fieldArea = 100*100;
+        const int fieldArea = 10000;
         int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
 
