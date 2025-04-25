d515171f4b65895b1db402eabf5802a9def4a6f4
Wardis-danang <94683362+Wardis-danang@users.noreply.github.com>
2025-04-25 09:50:50 +0800

    Update solution.cpp

--- 25-04-2025/Ha/solution.cpp
+++ 25-04-2025/Ha/solution.cpp
@@ -7,9 +7,9 @@ int main()
 	cin >> n;
 	for ( int i = 1; i <= n ; i ++ )
 	{
-		cin >> a1 >> a2 >> b1 >> b2;
-		cin >> c1 >> c2 >> d1 >> d2;
-		if ( b1 < c1 || a1 > d1 || b2 < c2 || a2 > d2 )
+		cin >> a1 >> a2 >> b1 >> b2; // a1, a2 ~ x1, y1 ; b1, b2 ~ x2, y2;
+		cin >> c1 >> c2 >> d1 >> d2; // c1, c2 ~ x3, y3 ; d1, d2 ~ x4, y4;	
+		if ( b1 < c1 || a1 > d1 || b2 < c2 || a2 > d2 ) // If no strong secured region 
 		{
 			res3 = ( b1 - a1 ) * ( b2 - a2 );
 			res4 = ( d1 - c1 ) * ( d2 - c2 );
@@ -17,6 +17,7 @@ int main()
 		}
 		else
 		{
+			// Put element into array to find max and min 
 			a[1] = a1;
 			a[2] = b1;
 			a[3] = c1;
@@ -25,10 +26,10 @@ int main()
 			b[2] = b2;
 			b[3] = c2;
 			b[4] = d2;
-			sort ( a + 1 , a + 1 + 4 );
+			sort ( a + 1 , a + 1 + 4 ); 	// If started in a[0] -> sort ( a, a + 4 ) 
 			sort ( b + 1 , b + 1 + 4 );
-			res = abs ( a[2] - a[3] );
-			res2 = abs ( b[2] - b[3] );
+			res = abs ( a[2] - a[3] );      // find distance of strong secured region of x
+			res2 = abs ( b[2] - b[3] );     // find distance of strong secured region of y
 			res3 = ( b1 - a1 ) * ( b2 - a2 );
 			res4 = ( d1 - c1 ) * ( d2 - c2 );
 			cout << res * res2 << " " << ( res3 + res4 ) - ( res * res2 ) * 2 << " " << 10000 - (( res3 + res4 ) - ( res * res2 ) * 2 + ( res * res2 )) << endl;
