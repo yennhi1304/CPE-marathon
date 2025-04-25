95c5d5b768170c931092f4be9c2ba9253422a6ef
yennhi1304 <yenhi130406@gmail.com>
2025-04-25 16:59:25 +0800

    Updated solution.cpp

---
+++ 26-04-2025/Nhi/solution.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int GCD(int a, int b)
+{
+    // Euclidean algorithm
+    return (b == 0) ? a : GCD(b, a % b);
+}
+
+// Another way to calculate the greatest common divisor
+// int GCD(int a, int b)
+// {
+//      return (a == b) ? a : (a > b) ? GCD(b, a - b) : GCD(b - a, a);
+// }
+
+int main()
+{
+    int N;
+    while(cin >> N)
+    {
+        if (N == 0) break; // If N == 0 , the input is terminated
+
+        // 
+        int G = 0;
+        for (int i = 1; i < N; i++)
+        {
+            for (int j = i + 1; j <= N; j++)
+            {
+                G += GCD(i, j);
+            }
+        }
+        cout << G << endl;
+
+    }
+    return 0;
+}
