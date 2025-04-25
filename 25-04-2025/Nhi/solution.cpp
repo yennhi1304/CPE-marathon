1ff3a15ee619f06f14ea528185d9d71b094dab0a
yennhi1304 <yenhi130406@gmail.com>
2025-04-25 16:17:08 +0800

    solution.cpp

--- 25-04-2025/Nhi/solution.cpp
+++ 25-04-2025/Nhi/solution.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+using namespace std;
+
+// A function to quickly calculate the area given coordinates
+int calculateArea(int x1, int y1, int x2, int y2)
+{
+    int length = x2 - x1;
+    int width = y2 - y1;
+    if (length <= 0 || width <= 0)  return 0;
+    return length*width;
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        const int fieldArea = 100*100;
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        int x3, y3, x4, y4;
+        cin >> x3 >> y3 >> x4 >> y4;
+
+        int stronglySecured = 0;
+        int weaklySecured = 0;
+        int unsecured = 0;
+
+        // Find the coordinates for the intersection part
+        int x_1 = max(x1, x3);
+        int y_1 = max(y1, y3);
+        int x_2 = min(x2, x4);
+        int y_2 = min(y2, y4);
+
+        stronglySecured = calculateArea(x_1, y_1, x_2, y_2); // calculateArea returns 0 if there is no intersection part
+
+
+        weaklySecured = calculateArea(x1, y1, x2, y2) + calculateArea(x3, y3, x4, y4) - 2*stronglySecured;
+        
+        unsecured = fieldArea - weaklySecured - stronglySecured;
+
+        cout << "Day " << i + 1 << ": " << stronglySecured << " " << weaklySecured << " " << unsecured << endl;
+    }
+    return 0;
+}
 
